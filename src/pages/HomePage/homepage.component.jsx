6fb6008cb8746ff9bef7591362a9ec4ac69b3317
Chandrakant <ckant912@gmail.com>
2022-04-21 11:24:30 +0530

    creating initial ecommerce app

---
+++ src/pages/HomePage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import './homepage.style.css'
+import Menu from '../../menu.component'
+
+const HomePage = () => {
+    return (
+        <>
+             <Menu />
+             <h1>Home Page</h1>
+        </>
+    )
+}
+
+
+
+export default HomePage
