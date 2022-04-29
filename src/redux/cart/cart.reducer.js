f2c2dfd0e18ff7a5220efaedf2b48bd9026f57a9
Chandrakant <ckant912@gmail.com>
2022-04-29 16:48:50 +0530

    making paymemnt gateway

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,29 @@
 import CART_ACTION_TYPES from "./cart.types";
+import { addItemToCart, clearItem , removeItem} from "./cart.utils";
 
 const INTIAL_STATE = {
-    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INTIAL_STATE, action) => {
     switch (action.type) {
-        case CART_ACTION_TYPES.TOGGLE_CART_HIDDEN:
-            return{
+        
+        case CART_ACTION_TYPES.ADD_ITEM:
+            return {
                 ...state,
-                hidden: !state.hidden
+                cartItems: addItemToCart(state.cartItems,action.payload)
             }
-    
+        case CART_ACTION_TYPES.CLEAR_ITEM:
+            return {
+                ...state,
+                cartItems: clearItem(state.cartItems,action.payload)
+            }
+
+        case CART_ACTION_TYPES.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItem(state.cartItems,action.payload)
+            }
         default:
             return state
     }
